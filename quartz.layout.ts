2b593001ed9d2e340de7e29a631e087086dc9776
Davaji <danyalhajili@gmail.com>
2024-06-30 20:14:59 +0330

    recent

--- quartz.layout.ts
+++ quartz.layout.ts
@@ -1,5 +1,6 @@
 import { PageLayout, SharedLayout } from "./quartz/cfg"
 import * as Component from "./quartz/components"
+import { SimpleSlug } from "./quartz/util/path"
 
 // components shared across all pages
 export const sharedPageComponents: SharedLayout = {
@@ -33,7 +34,7 @@ export const defaultContentPageLayout: PageLayout = {
 
     Component.RecentNotes({
       title: "Recent Notes",
-      limit: 1,
+      limit: 5,
       filter: (f) =>
         f.slug!.startsWith("posts/") && f.slug! !== "posts/index" && !f.frontmatter?.noindex,
       linkToMore: "posts/" as SimpleSlug,
